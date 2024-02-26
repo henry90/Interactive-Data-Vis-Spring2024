efec041352b6b6c51f4b93e821e3db0c5b49e55e
Yuxiao (Henry) Shen <yshen3@mail.yu.edu>
2024-02-25 22:12:58 -0500

    Section 1 | Tutorial 3

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,8 @@
 console.log('hello world');
+// Counter
+let count = 0; // Set the counter with 0
+
+function updateCount() {
+  count++; // Increment count by 1
+  document.getElementById("counter").innerText = count; // Update the number in the counter
+}
